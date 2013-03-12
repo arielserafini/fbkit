d51e73aa4a661586f854b8b3b1b412067b2838e2
Ariel Serafini <github@arielserafini.com>
2013-03-12 17:47:50 -0300

    add login status method

--- fbkit.js
+++ fbkit.js
@@ -11,17 +11,51 @@ var FBKit = (function(FB){
       console.log('Facebook Javascript SDK not found.');
     }
 
+    getLoginStatus();
+
     exports.login = login;
 
     return exports;
   };
 
+  var getLoginStatus = function() {
+    console.log('Getting Facebook login status...');
+
+    FB.getLoginStatus(function(response) {
+      status = response.status;
+
+      if (response.status === 'connected') {
+          // the user is logged in and has authenticated your
+          // app, and response.authResponse supplies
+          // the user's ID, a valid access token, a signed
+          // request, and the time the access token
+          // and signed request each expire
+          authResponse = response.authResponse;
+          user.id = authResponse.userID;
+          console.log(status, authResponse, user);
+
+        } else if (response.status === 'not_authorized') {
+          console.log('User is logged in, but did not authorize the app.');
+          login();
+          // authResponse = response.authResponse;
+          // user.id = authResponse.userID;
+          // console.log(status, authResponse, user);
+        } else {
+          // the user isn't logged in to Facebook.
+          console.log('User is not logged in to Facebook.');
+          login();
+        }
+      });
+
+  };
+
   // Handles login and permissions.
   var login = function(callback, params) {
-
+    console.log('Requesting login...');
     FB.login(function(response) {
       if (response.authResponse) {
-        // We're logged in.
+        console.log('Login/authorization successful!');
+
         if (callback) {
           callback.apply(FBKit, params);
         }
@@ -29,6 +63,7 @@ var FBKit = (function(FB){
         // Something went wrong.
       }
     });
+
   };
 
   return init();
