68e221349afb0cf5ede1fa71476059c29ee60f69
Ariel Serafini <github@arielserafini.com>
2013-03-12 13:46:14 -0300

    add checking for the FB object on initialization

--- fbkit.js
+++ fbkit.js
@@ -1,11 +1,16 @@
-var FBKit = (function(){
+var FBKit = (function(FB){
   var exports = {};
 
   var init = function() {
+
+    // Fails if the FB object is not found.
+    if (!FB) {
+      console.log('Facebook Javascript SDK not found.');
+    }
 
     return exports;
   };
 
   return init();
 
-}());
+}(window.FB));
